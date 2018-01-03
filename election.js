ed4876861a09f6d81c523897b86f6562ed61e23c
co2co2 <coco8287@hotmail.com>
2018-01-03 15:25:19 -0500

    done

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  // Imagination!
-
+  var candidates = document.querySelector('#candidates')
+  $.ajax({
+  url: "https://bb-election-api.herokuapp.com/",
+  methid: 'GET',
+  dataType: 'json',
+}).done(function(data) {
+  data.candidates.forEach(function(candidate){
+  var li = document.createElement('li')
+  li.innerHTML= (candidate.name+ ' : '+ candidate.votes)
+  candidates.append(li)
+    })
+  })
 });
